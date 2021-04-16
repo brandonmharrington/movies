0385f1d319cebda4e582856e8f773f8acc6e67ec
brandonmharrington <brandonmharrington@gmail.com>
2021-04-15 22:49:59 -0500

    fixed unnecessary import

--- src/components/SimilarMovies.js
+++ src/components/SimilarMovies.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import SimilarMovieCard from './SimilarMovieCard';
-import MovieCard from './MovieCard';
 import { MOVIE_URL, API_KEY } from '../config';
 
 const SimilarMovies = ({ movieId }) => {
@@ -10,7 +9,6 @@ const SimilarMovies = ({ movieId }) => {
     fetch(`${MOVIE_URL}${movieId}/similar?api_key=${API_KEY}`)
       .then(res => res.json())
       .then(data => setSimilarMovieData(data.results.slice(0, 8)));
-    console.log(similarMovieData);
   }, [movieId]);
 
   return (
