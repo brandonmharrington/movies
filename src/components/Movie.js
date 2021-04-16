e3efd9ada48b3efb09f44cd84db00a8330a186a7
brandonmharrington <brandonmharrington@gmail.com>
2021-04-15 22:24:27 -0500

    added similar movie component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Back from './Back';
+import SimilarMovies from './SimilarMovies';
 import { IMG_URL, ALT_IMG, MOVIE_URL, API_KEY } from '../config';
 
 const Movie = () => {
   const [movieData, setMovieData] = useState([]);
 
   const { movieId } = useParams();
+
+  const imdbUrl = `https://imdb.com/title/${movieData.imdb_id}`;
 
   useEffect(() => {
     fetch(`${MOVIE_URL}${movieId}?api_key=${API_KEY}`)
@@ -27,18 +30,37 @@ const Movie = () => {
           alt={movieData.title}
         ></img>
         <div className="movie-text">
-          <h1>{movieData.title}</h1>
-          <p>{movieData.release_date}</p>
-          <p>Runtime: {movieData.runtime} minutes</p>
-          <p>Average Rating: {movieData.vote_average}/10</p>
-          <p>
-            <b>Description</b>
+          <h1 className="movie-text-title">{movieData.title}</h1>
+          <p className="movie-text-release">
+            {movieData.release_date?.slice(0, 4)}
           </p>
-          <p>{movieData.overview}</p>
+          <p className="movie-text-runtime">
+            Runtime: {movieData.runtime} minutes
+          </p>
+          <p className="movie-text-rating">
+            Average Rating: {movieData.vote_average}/10
+          </p>
+          <a
+            className="movie-text-imdb"
+            href={imdbUrl}
+            target="_blank"
+            rel="noreferrer"
+          >
+            IMDb
+          </a>
+          <div className="movie-text-description">
+            <p>
+              <b>Description</b>
+            </p>
+            <p>{movieData.overview}</p>
+          </div>
           <a href={movieData.homepage} target="_blank" rel="noreferrer">
             More details...
           </a>
         </div>
+      </div>
+      <div className="similar-movies">
+        <SimilarMovies movieId={movieId} />
       </div>
     </>
   );
